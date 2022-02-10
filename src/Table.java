fbf89b8028f94811c78eaff7fd8bdc6e02cfb63f
ylose <ylose@DESKTOP-PC9EI8M>
2022-02-10 16:50:22 +0900

    테이블 생성하고 여러 사이즈 속성 적용

--- src/Table.java
+++ src/Table.java
@@ -1,5 +1,9 @@
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class Table {
 	public static void main(String[] args) {
@@ -9,9 +13,15 @@ public class Table {
 		Object[][] data = new Object[][] {
 			{"1","Daniel","Korea"},
 			{"2","Sunny","France"},
-			{"3","John","Japan"};
+			{"3","John","Japan"}
 		};
 		
+		JTable table = new JTable(data, headings);
+		table.setPreferredScrollableViewportSize(new Dimension(700, 600));
+		
+		table.setFillsViewportHeight(true);
+		
+		panel.add(new JScrollPane(table));
 		
 		frame.add(panel);
 		
