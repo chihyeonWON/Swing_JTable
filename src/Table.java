8dd2ef4d8e833f64f4270bba4eca92c4ac794fe2
ylose <ylose@DESKTOP-PC9EI8M>
2022-02-10 16:41:31 +0900

    테이블이 들어갈 패널 생성

--- src/Table.java
+++ src/Table.java
@@ -1,8 +1,12 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Table {
 	public static void main(String[] args) {
 		JFrame frame =  new JFrame("Table Prac.");
+		JPanel panel = new JPanel();
+		
+		
 		frame.setVisible(true); // 보여지도록설정
 		frame.setSize(800, 640); //크기 설정
 		frame.setLocationRelativeTo(null); // 위치제한해제
