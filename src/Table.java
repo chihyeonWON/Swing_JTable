770524158288f619a6aa7a7cfb95e348a34a157f
ylose <ylose@DESKTOP-PC9EI8M>
2022-02-10 16:40:49 +0900

    창 기본 설정

--- src/Table.java
+++ src/Table.java
@@ -1,4 +1,11 @@
+import javax.swing.JFrame;
 
 public class Table {
-
+	public static void main(String[] args) {
+		JFrame frame =  new JFrame("Table Prac.");
+		frame.setVisible(true); // 보여지도록설정
+		frame.setSize(800, 640); //크기 설정
+		frame.setLocationRelativeTo(null); // 위치제한해제
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //종료했을  시스템종료되도록 설정
+	}
 }
